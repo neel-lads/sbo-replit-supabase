30782e3894656dd768eadcc598ef4261644cacc6
Neel Ladani <neel.lads2004@gmail.com>
2026-06-21 14:26:05 +0530

    hardcode

--- artifacts/sardar-bio/src/lib/supabase.ts
+++ artifacts/sardar-bio/src/lib/supabase.ts
@@ -1,12 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-
-console.log("ENV CHECK:", { supabaseUrl, supabaseAnonKey });
-
-if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error("Missing Supabase ENV");
-}
+const supabaseUrl = "https://pfdwgxzhdqtvedwiovtn.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBmZHdneHpoZHF0dmVkd2lvdnRuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE4NDg0MDgsImV4cCI6MjA5NzQyNDQwOH0.23ghiieXZlslFh01hdbv7TAuyMbXYlqjxP7wQDMcPFE";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
