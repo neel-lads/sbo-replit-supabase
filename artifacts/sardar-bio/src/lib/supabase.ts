df8823abc0fc2caf6bcd91de0239d7921cc77634
Neel Ladani <neel.lads2004@gmail.com>
2026-07-03 18:44:20 +0530

    database change

--- artifacts/sardar-bio/src/lib/supabase.ts
+++ artifacts/sardar-bio/src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://pfdwgxzhdqtvedwiovtn.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBmZHdneHpoZHF0dmVkd2lvdnRuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE4NDg0MDgsImV4cCI6MjA5NzQyNDQwOH0.23ghiieXZlslFh01hdbv7TAuyMbXYlqjxP7wQDMcPFE";
+const supabaseUrl = "https://pelqbtytbnxriifwyjfu.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBlbHFidHl0Ym54cmlpZnd5amZ1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMwNjQzNzYsImV4cCI6MjA5ODY0MDM3Nn0.n3DuGfAjZO8GaNujO3bGaBbxRyYP3aSYHezlKsMCJRI";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
